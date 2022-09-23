df85a3963e70551a8446b4a05c9526c715b529aa
Gavin Wentzel <gavinwentzel@gmail.com>
2022-09-23 16:30:18 -0600

    Fixed navbar appearance

--- Client/our-village-roots/src/App.js
+++ Client/our-village-roots/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <header>
+        <Navbar />
+
+    </header>
+      
+        
+        <h2>
+              Serving Mama Africa… One Village At A Time
+        </h2>
+        <h3>Ndoucouman, Sengegal</h3>
+      
+   
+    </>
   );
 }
 
--- Client/our-village-roots/src/components/Navbar.js
+++ Client/our-village-roots/src/components/Navbar.js
@@ -1,26 +1,30 @@
 import React, {Component} from "react";
 import { Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import '../css/Navbar.css';
 
 export default class Navbar extends Component {
 
     render(){
         return(
-          <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="#">Our Village Roots</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-item nav-link active" href="#"> <span class="sr-only">(current)</span></a>
-              <a class="nav-item nav-link" href="#">History</a>
-              <a class="nav-item nav-link" href="#">Vision</a>
-              <a class="nav-item nav-link" href="#">About Us</a>
-              <a class="nav-item nav-link" href="#">Contact</a>
-              <a class="nav-item nav-link" href="#">Donation</a>
+          <nav className="d-flex navbar navbar-expand-lg bg-light ">
+            <div className="d-flex ">
+                  <a className="navbar-brand" href="#">Our Village Roots</a>
             </div>
-          </div>
+              
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+              <div className="d-flex collapse navbar-collapse ml-auto flex-row-reverse" id="navbarNavAltMarkup">
+                  <div className="d-flex navbar-nav ml-auto flex-row">
+                      <a className="nav-item nav-link active" href="#"> <span className="sr-only">(current)</span></a>
+                      <a className="nav-item nav-link" href="#">History</a>
+                      <a className="nav-item nav-link" href="#">Vision</a>
+                      <a className="nav-item nav-link" href="#">About Us</a>
+                      <a className="nav-item nav-link" href="#">Contact</a>
+                      <a className="nav-item nav-link" href="#">Donation</a>
+                  </div>
+              </div>
         </nav>
  
         )
